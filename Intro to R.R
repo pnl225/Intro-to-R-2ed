01beff45cf2fa04b67d1d0f46c789313afad0870
Peyton Levine <pnl225@lehigh.edu>
2021-08-29 17:15:05 -0400

    Edited name

--- Intro to R.R
+++ Intro to R.R
@@ -1,5 +1,5 @@
 # R script to accompany Intro to R
-# Copy for Peyton Levine
+# Copy for Peyton N. Levine
 ######################################################
 
 # You will see that you can put non-executable "comment" lines
